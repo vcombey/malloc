9393f797dba6cb240e81dae0ead0cdd8d56d6ec4
Vincent COMBEY <vcombey@student.42.fr>
2018-05-12 20:42:20 +0200

    testx

--- shared_test/testX.c
+++ shared_test/testX.c
@@ -18,10 +18,24 @@ int main()
 
 		printf("bonjour %i\n", i);
 		if (nb % 3 == 0)
+		{
 			tab[nb % 1000] = malloc(nb);
+			if (tab[nb % 1000] != NULL)
+			{
+				((char*)tab[nb % 1000])[nb - 1] = '*';
+				((char*)tab[nb % 1000])[0] = '*';
+			}
+		}
 		else if (nb % 3 == 1)
 			free(tab[nb % 1000]);
 		else if (nb % 3 == 2)
+		{
 			tab[nb % 1000] = realloc(tab[nb % 1000], nb);
+			if (tab[nb % 1000] != NULL)
+			{
+				((char*)tab[nb % 1000])[nb - 1] = '*';
+				((char*)tab[nb % 1000])[0] = '*';
+			}
+		}
 	}
 }
